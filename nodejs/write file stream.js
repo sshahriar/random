acf1641a1c8653e44299aacfabf7b1d97ffcaa69
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-03-01 10:34:09 +0600

    Update write file stream.js

--- nodejs/write file stream.js
+++ nodejs/write file stream.js
@@ -5,15 +5,17 @@ var fs =  require( 'fs') ;
 var myReadStream = fs.createReadStream( __dirname +  '/readme.txt' ,'utf8' )
 var myWriteStream = fs.createWriteStream( __dirname +  '/writeme.txt' ,'utf8')
 
-myReadStream.on ('data' , function( chunk ) {
-    console.log('new chunkn received' ) ; 
-    console.log(chunk  ) ; 
-    myWriteStream.write( chunk ) ;   // writes  chunk received  to writeme.txt 
+// myReadStream.on ('data' , function( chunk ) {
+//     console.log('new chunkn received' ) ; 
+//     console.log(chunk  ) ; 
+//     myWriteStream.write( chunk ) ;   // writes  chunk received  to writeme.txt 
 
-})  ; 
+// })  ; 
 
 
 
+ // does same job  as above 
+ myReadStream.pipe(myWriteStream )  ; 
 
 
 
