0281adba61a7a6cab486afd593b81004c24b488b
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-03-01 10:11:49 +0600

    Create server create.js

---
+++ nodejs/server create.js
@@ -0,0 +1,16 @@
+var  http = require('http') ;   //  module  
+
+var server =  http.createServer(function(req,res){
+    
+    console.log('request was made ' + req.url ) ;
+
+    res.writeHead(200,{'Content-Type': 'text/plain' }) ; // ( status ok or not , {content type } )
+    
+    res.end('hello  !! ') ; 
+}) ; 
+
+
+server.listen(3002 ,'127.0.0.1') ;  // listen( port number , ip address )
+console.log('listening to port 3000 ' )  ; 
+
+c
