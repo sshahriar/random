bdaf79a9421b3881ff6680a0c84c58ae7ef7be7c
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-03-01 11:30:42 +0600

    Create send json data.js

---
+++ nodejs/send json data.js
@@ -0,0 +1,18 @@
+
+var fs =  require( 'fs') ; 
+var  http = require('http') ; 
+
+var server =  http.createServer(function(req,res){
+    console.log('request was made ' + req.url ) ;
+    //  writehead( status ok or not , {content type } )
+    res.writeHead(200,{'Content-Type': 'text/html' }) ; 
+
+
+var myObj =  {
+    name: 'sn'  , 
+    job: 'sfgds'  , 
+    age: '25' ,  
+} ; 
+
+res.end(JSON.stringify(  myObj  )   ) ; 
+    
